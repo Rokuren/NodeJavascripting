b6bcc948613ee36469d5b66b0d9a5b5a56e23ee7
Chris <rokuren.m@gmail.com>
2016-01-25 15:14:58 -0500

    bug-clinic started

---
+++ bugclinic/intake.js
@@ -0,0 +1,9 @@
+/*
+ * console.trace(label): dump stacktrace at line trace is called, with label
+ * console.time(label) / console.timeEnd(label): quick & dirty benchmarking
+ * console.assert(expression, arguments): assert that expression is true
+ * console.dir(object): call util.inspect() on `object`, print results
+ */
+
+console.log("i am okay");
+console.error("I am so incredibly not okay");
